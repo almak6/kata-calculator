345901f5742f09807f20399a79a4e81ac736b148
Alexander Maksimov <maksimovi.ck@mail.ru>
2022-05-16 19:53:44 +0300

    test commit

--- src/com/company/ArabicNumber.java
+++ src/com/company/ArabicNumber.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class ArabicNumber {
 
-    public static String[] ARR_AR_NUM = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    public static String[] ARR_AR_NUM = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
 
 
     public static void calculationArabicNumber(String num1, String num2, String operator) {
